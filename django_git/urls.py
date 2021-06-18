d7a947823dba4f35924deb72e71af104b5b23179
Francisco Menezes <franciscofm.menezes@gmail.com>
2021-06-18 10:57:55 -0300

    Configurando settings para o app blog

--- django_git/urls.py
+++ django_git/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sobre/', include('sobre.urls')),
+    path('blog/', include('blog.urls')),
 ]
